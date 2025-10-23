842ceb51ca1bf90dd446678869552a69babf0558
BlackDabt <151771884+BlackDabt@users.noreply.github.com>
2025-10-23 21:59:11 +0300

    Adaugat coentariu in index.html

---
+++ p2/script.js
@@ -0,0 +1,9 @@
+function varsta(){
+    let ani = prompt("Introuceti varsta:");
+    if(ani >= 18){
+        console.log("Poti obtine permisul de conducere");
+    }
+    else{
+        console.log("Nu poti obtine permisul de conducere");
+    }
+}
